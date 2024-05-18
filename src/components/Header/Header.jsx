5ecc510d02a74643e68df17df0682a7c798a7d44
Julie <juliegodoi@gmail.com>
2024-05-18 15:51:45 -0300

    style: ajusta nav e header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,9 @@ import Header from './Header.css';
 export default function Headerr() {
   return (
     <div className={Header}>
-      <div className="container-fluid position-relative">
-        <img src={HeaderImg} className="img-fluid object-fit-fill" alt="..." />
-        <div class="position-absolute top-50 start-50 translate-middle text-start text-white">
+      <div className="container-fluid header position-relative">
+        <img src={HeaderImg} className="img-fluid" alt="..." />
+        <div className="position-absolute top-50 start-50 translate-middle text-start text-white">
           <h5 className="display-3">Ajuda humanitária</h5>
           <p className="fs-5"> O nosso trabalho visa comunidades indígenas em situação de vulnerabilidade social. Combatemos a fome, erradicamos pobreza e oferecemos programas de geração de renda.
           </p>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,29 +6,55 @@ import Logo from '../../assets/images/Logo.png';
 export default function Navbarr() {
   const location = useLocation();
   return (
-    <div className={Navbar}>
-      <nav className="navbar navbar-expand-lg bg-transparent fixed-top font-regular">
-        <div className="container">
-        <img src={Logo} alt="Logo Ajude O Pantanal"/>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav m-auto gap-4 me-5">
-              <li className="nav-item">
-                <Link className={`nav-link fs-5 text-white ${location.pathname === '/' ? 'active' : ''}`} to="/">Início</Link>
-              </li>
-              <li className="nav-item">
-                <Link className={`nav-link fs-5 text-white ${location.pathname === '/quem-somos' ? 'active' : ''}`} to="/quem-somos">Quem Somos</Link>
-              </li>
-              <li className="nav-item">
-                <Link className={`nav-link fs-5 text-white ${location.pathname === '/nossos-projetos' ? 'active' : ''}`} to="/nossos-projetos">Nossos Projetos</Link>
-              </li>
-            </ul>
-            <button type="button" className="btn btn-outline-light rounded-3 me-5">Ajude-nos agora</button>
+    <>
+      {/*nav grande*/}
+      <div className={Navbar}>
+        <nav className="navbar navbar-expand-lg bg-transparent fixed-top font-regular d-none d-lg-block nav-large">
+          <div className="container">
+            <img src={Logo} alt="Logo Ajude O Pantanal"/>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav m-auto gap-4 me-5">
+                <li className="nav-item">
+                  <Link className={`nav-link fs-5 text-white ${location.pathname === '/' ? 'active' : ''}`} to="/">Início</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={`nav-link fs-5 text-white ${location.pathname === '/quem-somos' ? 'active' : ''}`} to="/quem-somos">Quem Somos</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={`nav-link fs-5 text-white ${location.pathname === '/nossos-projetos' ? 'active' : ''}`} to="/nossos-projetos">Nossos Projetos</Link>
+                </li>
+              </ul>
+              <button type="button" className="btn btn-outline-light rounded-3 me-5">Ajude-nos agora</button>
+            </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
+      {/*nav pequena média*/}
+      <nav className="navbar navbar-expand-lg bg-light fixed-top font-regular d-lg-none">
+          <div className="container">
+            <img src={Logo} alt="Logo Ajude O Pantanal"/>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className={`nav-link fs-5 text-black ${location.pathname === '/' ? 'active' : ''}`} to="/">Início</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={`nav-link fs-5 text-black ${location.pathname === '/quem-somos' ? 'active' : ''}`} to="/quem-somos">Quem Somos</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={`nav-link fs-5 text-black ${location.pathname === '/nossos-projetos' ? 'active' : ''}`} to="/nossos-projetos">Nossos Projetos</Link>
+                </li>
+              </ul>
+              <button type="button" className="btn btn-outline-dark rounded-3 me-5">Ajude-nos agora</button>
+            </div>
+          </div>
+        </nav>
+    </>
   );
 }
