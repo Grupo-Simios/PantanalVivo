814cd0868568eca81d355a678cfa421aa0b1cd9a
Julie <juliegodoi@gmail.com>
2024-05-26 20:48:45 -0300

    style: ajusta componente nossa equipe

--- src/components/Inicio/NossaEquipe/index.jsx
+++ src/components/Inicio/NossaEquipe/index.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 export default function NossaEquipe() {
   return (
-    <div className="container-fluid">
+    <div className="container">
       <div className="row w-75 bg-dark-orange shadow rounded-4 font-regular mb-5 mt-4">
-        <div className="col-2 pt-5 pb-5 ps-5">
+        <div className="col-3 pt-5 pb-5 ps-5">
             <h2>Nossa equipe</h2>
             <p>Conheça quem faz tudo isso acontecer</p>
         </div>
         <div className="col">
+
         </div>
       </div>
     </div>
