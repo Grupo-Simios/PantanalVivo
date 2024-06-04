a72b9b0f9895af095e8690fb6a800711297219b5
Julie <juliegodoi@gmail.com>
2024-06-04 15:50:32 -0300

    refactor: ajusta header conforme design[

--- src/components/Inicio/Header/index.jsx
+++ src/components/Inicio/Header/index.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-// import HeaderImg from '../../../assets/images/HeaderImg.png';
+import HeaderImg from '../../../assets/images/HeaderImg.png';
 import Style from './style.css';
 
 export default function Header() {
   return (
     <div className={Style}>
-      <header className='vh-100 d-flex position-relative'>
-        <div className="container-fluid">
-          <div className="row justify-content-center align-items-center text-center text-white overlay position-absolute">
-            <div className="col-8">
-              <h5 className="display-3">Ajuda humanitária</h5>
-              <p className="fs-5">O nosso trabalho visa comunidades indígenas em situação de vulnerabilidade social. Combatemos a fome, erradicamos pobreza e oferecemos programas de geração de renda.</p>
-            </div>
+      <div id="carouselHeader" className="carousel slide">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={HeaderImg} className="d-block w-100" alt="..."/>
           </div>
+          {/*<div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..."/>
+          </div>*/}
         </div>
-      </header>
+        <div className="carousel-caption d-none d-md-block text-start position-absolute start-0 ms-5 ps-3 w-50">
+          <h1 className='font-bold display-3'>Ajuda humanitária</h1>
+          <p className='font-regular fs-4'>
+            O nosso trabalho visa comunidades indígenas em situação de vulnerabilidade social. <br /> 
+            Combatemos a fome, erradicamos pobreza e oferecemos programas de geração de renda.
+          </p>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselHeader" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselHeader" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
   );
 }
