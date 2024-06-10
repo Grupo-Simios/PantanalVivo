d166f68ae6633217eb356c6b05c6c57123c63c6c
Julie <juliegodoi@gmail.com>
2024-06-10 12:19:21 -0300

    refactor: ajusta menu mobile

--- src/components/Global/Navbar/index.jsx
+++ src/components/Global/Navbar/index.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import {Link, useLocation} from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import Style from './style.css';
 import Logo from '../../../assets/images/Logo.svg';
 
@@ -7,32 +7,67 @@ export default function Navbar() {
   const location = useLocation();
 
   return (
-      <div className={Style}>
-        <nav className="navbar navbar-expand-md bg-transparent font-regular nav-large position-absolute w-100 top-0 z-3 shadow rounded-bottom-4">
-          <div className="container-fluid px-5 mx-3">
-            <img src={Logo} className="rounded-circle" alt="Logo Ajude O Pantanal"/>
-            <button className="navbar-toggler rounded-circle p-2 border border-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span><i className='bi bi-list fs-1 text-white'></i></span>
+    <div className={Style}>
+      {/*nav lg*/}
+      <nav className="navbar navbar-expand-md bg-transparent font-regular nav-large position-absolute w-100 top-0 z-3 shadow rounded-bottom-4">
+        <div className="container-fluid px-5 mx-3">
+          <img src={Logo} className="rounded-circle" alt="Logo Ajude O Pantanal"/>
+          <button className="navbar-toggler rounded-circle p-2 border border-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span>
+              <i className="bi bi-list fs-1 text-white"></i>
+            </span>
+          </button>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ms-auto gap-5 me-5">
+              <li className="nav-item">
+                <Link className={`nav-link fs-5 text-white ${location.pathname === '/' ? 'active' : ''}`} to="/">Início</Link>
+              </li>
+              <li className="nav-item">
+                <Link className={`nav-link fs-5 text-white ${location.pathname === '/quem-somos' ? 'active' : ''}`} to="/quem-somos">
+                  Quem Somos
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className={`nav-link fs-5 text-white ${location.pathname === '/nossos-projetos' ? 'active' : ''}`} to="/nossos-projetos">
+                  Nossos Projetos
+                </Link>
+              </li>
+              <li className="nav-item d-block d-md-none">
+                <Link className="nav-link fs-5 text-white" to="/doar">Doe agora</Link>
+              </li>
+            </ul>
+            <button type="button" className="btn btn-outline-light btn-lg custum-btn rounded-5 ps-4 pe-4 fw-bold d-none d-md-block">
+              Doe agora
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav m-auto gap-3 gap-lg-5 me-lg-5 pt-4 pb-4 pt-md-0 pb-md-0">
-                <li className="nav-item">
-                  <Link className={`nav-link fs-5 text-white ${location.pathname === '/' ? 'active' : ''}`} to="/">Início</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className={`nav-link fs-5 text-white ${location.pathname === '/quem-somos' ? 'active' : ''}`} to="/quem-somos">Quem Somos</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className={`nav-link fs-5 text-white ${location.pathname === '/nossos-projetos' ? 'active' : ''}`} to="/nossos-projetos">Nossos Projetos</Link>
-                </li>
-                <li className='nav-item'>
-                  <Link className="nav-link fs-5 text-white d-block d-md-none">Doe agora</Link>
-                </li>
-              </ul>
-              <button type="button" className="btn btn-outline-light btn-lg custum-btn rounded-5 ps-4 pe-4 fw-bold d-none d-md-block">Doe agora</button>
-            </div>
           </div>
-        </nav>
+        </div>
+      </nav>
+      {/*nav sm*/}
+      <div className="offcanvas offcanvas-end rounded-5 shadow h-75" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div className="offcanvas-header">
+          <button type="button" className="btn-close fs-2 btn-close-white rounded-circle pt-5 ps-5 " data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div className="offcanvas-body ps-5 pe-5">
+          <ul className="navbar-nav m-auto gap-3">
+            <li className="nav-item">
+              <Link className={`nav-link text-white ${location.pathname === '/' ? 'active' : ''}`} to="/">Início</Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link text-white ${location.pathname === '/quem-somos' ? 'active' : ''}`} to="/quem-somos">
+                Quem Somos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link text-white ${location.pathname === '/nossos-projetos' ? 'active' : ''}`} to="/nossos-projetos">
+                Nossos Projetos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/">Doe agora</Link>
+            </li>
+          </ul>
+        </div>
       </div>
+    </div>
   );
 }
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Container from '../components/Global/Container/index.jsx';
 import Navbar from '../components/Global/Navbar/index.jsx';
 import Footer from '../components/Global/Footer/index.jsx';
