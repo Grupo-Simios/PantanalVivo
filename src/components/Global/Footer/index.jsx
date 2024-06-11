1dfa549738053cece66178626bcb6b47294815b6
Julie <juliegodoi@gmail.com>
2024-06-10 22:01:22 -0300

    style: inicia ajustes no footer

--- src/components/Global/Footer/index.jsx
+++ src/components/Global/Footer/index.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import LogoFooterImg from '../../../assets/images/LogoFooterImg.png';
+import LogoFooter from '../../../assets/images/LogoFooter.svg';
 
 export default function Footer() {
   return (
     <footer className="bg-footer">
-      <div className="container font-regular pt-5 pb-5">
+      <div className="container font-bold pt-5 pb-5">
         <div className="row align-items-center justify-content-center">
           <div className="col-3">
-            <img src={LogoFooterImg} className="img-fluid" alt="..." />
-            <p className="text-green">Frase de efeito</p>
+            <img src={LogoFooter} className="img-fluid" alt="..." />
+            <p className="text-green">Juntos construímos esperança</p>
           </div>
-          <div className="col-3 text-white">
+          <div className="col-3 text-green">
             <h3>Link</h3>
             <p>Inicio</p>
             <p>Quem Somos</p>
             <p>Nossos Projetos</p>
           </div>
-          <div className="col-3 text-white">
+          <div className="col-3 text-green">
             <h3>Contato</h3>
-            <p>Redes Sociais</p>
-            <p>Redes Sociais</p>
-            <p>Redes Sociais</p>
+            <p>E-mail: email@email.com</p>
+            <p>Telefone: +55 99 99999-9999</p>
+            <p>Instagram: @ajude_o_pantanaloficial</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <hr className=''/>
+            <p className='text-white'>Todos os direitos reservados para Grupo Símios</p>
           </div>
         </div>
       </div>
