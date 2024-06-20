576b6b8eb3abe14652097303c66ff565028749cc
Leonarda Oliveira <120137983+zyzleoz@users.noreply.github.com>
2024-06-20 14:51:10 -0300

    refactor: ajusta tamanho das logos

--- src/components/Inicio/Parceiros/index.jsx
+++ src/components/Inicio/Parceiros/index.jsx
@@ -2,30 +2,36 @@ import React from 'react';
 import LoremIpsum from '../../../assets/images/LoremIpsum.png';
 import TechLogo from '../../../assets/images/TechLogo.png';
 import NameLogo from '../../../assets/images/NameLogo.png';
+import Style from './style.css';
 
 
 
 export default function Parceiros() {
   return (
-    <div className="container-fluid bg-light-orange rounded-4 mt-5 mb-5 shadow">
-      <div className="row pt-4 ">
-        <div className="col font-bold text-green text-start ps-5">
-          <h2>Parceiros</h2>
-        </div>
-      </div>
-      <div className=" pb-4">
-        <div className='d-flex container align-items-center justify-content-center'>
-          <div className=' img-fluid row '>
-            <img src={NameLogo} alt="Logo do Parceiro" />
-          </div>
-          <div className=' img-fluid row '>
-            <img src={TechLogo} alt="Logo do Parceiro" />
-          </div>
-          <div className=' img-fluid row'>
-            <img src={LoremIpsum} alt="Logo do Parceiro" />
+    <div className={Style}>
+      <div className="container-fluid bg-light-orange rounded-4 mt-5 mb-5 shadow">
+        <div className="row pt-4 ">
+          <div className="col font-bold text-green text-start ps-5">
+            <h2>Parceiros</h2>
           </div>
         </div>
-        
+        <div className=" pb-4">
+          <div className='d-flex container align-items-center justify-content-around'>
+            <div className='logoname img-fluid row flex-nowrap align-items-center'>
+              <img src={NameLogo} alt="Logo do Parceiro" />
+              <p className='text-green text-start font-regular fs-5'>NameLogo</p>
+            </div>
+            <div className='logotech img-fluid row flex-nowrap align-items-center '>
+              <img src={TechLogo} alt="Logo do Parceiro" />
+              <p className='text-green text-start font-regular fs-5'>TechLogo</p>
+            </div>
+            <div className='logolorem img-fluid row flex-nowrap align-items-center'>
+              <img src={LoremIpsum} alt="Logo do Parceiro" />
+              <p className='text-green text-start font-regular fs-5'>LoremIpsum</p>
+            </div>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
