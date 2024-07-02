1ac287e1f91f812f05326b3b2437ac82d1b04bef
Julie <juliegodoi@gmail.com>
2024-07-01 23:55:18 -0300

    refactor: ajusta footer desktop

--- src/components/Global/Footer/index.jsx
+++ src/components/Global/Footer/index.jsx
@@ -1,35 +1,57 @@
 import React from 'react';
-import LogoFooter from '../../../assets/images/LogoFooter.svg';
+import Style from './style.css';
+import LogoFooter from '../../../assets/images/LogoFooter.png';
+import IconeLiGs from '../../../assets/images/IconeLiGs.svg';
+import IconeIgGs from '../../../assets/images/IconeIgGs.svg';
+import IconeIgContato from '../../../assets/images/IconeIgContato.svg';
+import IconeWppContato from '../../../assets/images/IconeWppContato.svg';
 
 export default function Footer() {
   return (
-    <footer className="bg-footer">
-      <div className="container font-bold pt-5 pb-5">
-        <div className="row align-items-center justify-content-center">
-          <div className="col-3">
-            <img src={LogoFooter} className="img-fluid" alt="..." />
-            <p className="text-green">Juntos construímos esperança</p>
+    <div className={Style}>
+      <footer className="bg-footer">
+        <div className="container pt-5 pb-5">
+          <div className="row font-bolder pb-3">
+            <div className="d-flex justify-content-between align-items-center w-100">
+              <div className="col-4 align-items-start">
+                <img src={LogoFooter} className="img-fluid" alt="..." />
+                <p className="text-green fs-5">Juntos reconstruímos esperança</p>
+              </div>
+              <div className="col-2 text-green">
+                <h3 className='fw-bold'>Links</h3>
+                <p className='mb-0 pb-2'>Inicio</p>
+                <p className='mb-0 pb-2'>Quem Somos</p>
+                <p className='mb-0'>Nossos Projetos</p>
+              </div>
+              <div className="col-4 text-green">
+                <h3 className='fw-bold'>Contato</h3>
+                <p>E-mail: email@email.com</p>
+                <div className='d-flex gap-1 pb-2'>
+                  <img src={IconeWppContato} width={24} height={24} className="img-fluid" alt="Icone de WhatsApp" />
+                  <p className='mb-0'>Telefone: +55 99 99999-9999</p>
+                </div>
+                <div className='d-flex gap-1'>
+                  <img src={IconeIgContato} width={24} height={24} className="img-fluid" alt="Icone de Instagram" />
+                  <p className='mb-0'>Instagram: @ongpantanalvivo</p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col-3 text-green">
-            <h3>Link</h3>
-            <p>Inicio</p>
-            <p>Quem Somos</p>
-            <p>Nossos Projetos</p>
-          </div>
-          <div className="col-3 text-green">
-            <h3>Contato</h3>
-            <p>E-mail: email@email.com</p>
-            <p>Telefone: +55 99 99999-9999</p>
-            <p>Instagram: @ajude_o_pantanaloficial</p>
+          <div className="row">
+            <div className="divider mt-3 mb-3"/>
+            <div className="col align-items-start">
+              <p className='text-white font-regular'>O site em desenvolvimento não possui função comercial, sendo destinado apenas para fins de estudo. Todas as imagens utilizadas estão devidamente creditadas aos seus respectivos proprietários.</p>
+            </div>
+            <div className="col d-flex justify-content-end align-items-center gap-2">
+              <div className="d-flex align-items-center gap-1">
+                <img src={IconeLiGs} width={24} height={24} className="img-fluid" alt="Icone de LinkedIn" />
+                <img src={IconeIgGs} width={24} height={24} className="img-fluid" alt="Icone de Instagram" />
+              </div>
+              <p className='text-white font-bolder mb-0'>Todos os direitos reservados para Grupo Símios</p>
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <hr className=''/>
-            <p className='text-white'>Todos os direitos reservados para Grupo Símios</p>
-          </div>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 }
