c581a93d9855475468c583caa0668bf7a28945e4
Julie <juliegodoi@gmail.com>
2024-07-05 18:43:52 -0300

    refactor: finaliza header pag inicio

--- src/components/Inicio/Header/index.jsx
+++ src/components/Inicio/Header/index.jsx
@@ -10,20 +10,22 @@ export default function Header() {
       <div id="carouselHeader" className="carousel slide">
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src={HeaderImg1} className="d-block w-100 img-fluid" alt="..."/>
+            <img src={HeaderImg1} className="d-block w-100 img-fluid" alt="Menino indígena"/>
           </div>
           <div className="carousel-item">
-            <img src={HeaderImg2} className="d-block w-100 img-fluid" alt="..."/>
+            <img src={HeaderImg2} className="d-block w-100 img-fluid" alt="Jacarés"/>
           </div>
           <div className="carousel-item">
-            <img src={HeaderImg3} className="d-block w-100 img-fluid" alt="..."/>
+            <img src={HeaderImg3} className="d-block w-100 img-fluid" alt="Piranha"/>
           </div>
         </div>
         <div className="carousel-caption d-none d-md-block text-start position-absolute start-0 ms-5 ps-3 w-50">
           <h1 className='font-bold display-3'>Ajuda humanitária</h1>
           <p className='font-regular fs-4'>
-            O nosso trabalho visa comunidades indígenas em situação de vulnerabilidade social. <br /> 
-            Combatemos a fome, erradicamos pobreza e oferecemos programas de geração de renda.
+            O nosso trabalho tem como principal objetivo ajudar
+            as comunidades afetadas pelas queimadas no Pantanal,
+            promovendo a recuperação ambiental,
+            socioeconômica e cultural da região.
           </p>
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselHeader" data-bs-slide="prev">
