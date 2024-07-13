4842244fe22b159b77041e5071b4bd51d4bed3e4
Juan Henrique <juanmisidoro@gmail.com>
2024-07-13 04:14:17 -0300

    style: incluir detalhes e responsividade à página

--- src/components/QuemSomos/Body/index.jsx
+++ src/components/QuemSomos/Body/index.jsx
@@ -2,18 +2,43 @@ import React from "react";
 import Style from "./style.css";
 
 export default function BodyQss() {
-    return(
+    return (
         <div className={Style}>
             <div className="bodyQs pt-5">
-                <div className=" d-flex justify-content-end rounded-start-3 fundo-tittle position-absolute start-50 end-0 mt-4">
-                    <h1 className=" text-center pt-2 pb-2 ps-5 pe-5 tittleQs ">Nossa História</h1>
+                <div className="rounded-start-3 section-title fundo-title position-absolute end-0 mt-5">
+                    <h2 className=" text-start text-md-end pe-md-5 py-2 px-5 titleQs ">Nossa História</h2>
                 </div>
-                <div className="row container contNs pt-5 align-items-center rounded-end-4 gap-5 ps-5">
-                    <div className="col-6 TextNh pt-5 mt-4 pb-3">
-                        <p>Somos uma Organização da Sociedade Civil  (OSC) com sede em SP -Capital e sub sede em Miranda - MS. <br/>Formada por indígenas e não indígenas, atuamos junto aos Povos Originários em situação de vulnerabilidade social na região pantaneira.</p>
+                <div className="row contNs align-items-center rounded-end-4 gap-5 ps-4 mb-5 px-md-5">
+                    <div className="col-12 col-md-5 TextNh">
+                        <p>Somos uma Organização da Sociedade Civil  (OSC) com sede em SP -Capital e sub sede em Miranda - MS. <br />Formada por indígenas e não indígenas, atuamos junto aos Povos Originários em situação de vulnerabilidade social na região pantaneira.</p>
                     </div>
-                    <div className="col-6 TextNh pt-5 mt-4 pb-3">
+                    <div className="col-12 col-md-6 TextNh ">
                         <p>O IAPO nasceu como Movimento AJude o Pantanal Oficial em Setembro de 2020 na Terra Indígena Baía dos Guató - MS, a partir de uma situação emergencial onde 83% da aldeia foi consumida pelo fogo, ocorrendo assim a maior tragédia ambiental e social no Pantanal , gerando prejuízos ambientais, econômicos e sociais sem precedentes, em Abril de 2022 constituímos a nossa OSC.</p>
+                    </div>
+                </div>
+                <div className="position-relative section__nossos-propositos">
+                    <div className="position-absolute section__nossos-propositos-list rounded-start-4 end-0 text-white py-5 px-md-5">
+                        <ul className="list__nossos-propositos">
+                            <li className="d-md-flex pb-4">
+                                <h3>Nossa <br />missão</h3>
+                                <p className="px-md-5 fs-4">Promover a restauração do ecossistema do Pantanal, apoiar às comunidades afetadas pelas queimadas e incentivar práticas sustentáveis visando a preservação do bioma e a qualidade de vida.</p>
+                            </li>
+                            <li className="d-md-flex pb-4">
+                                <h3>Nossa <br />visão</h3>
+                                <p className="px-md-5 fs-4">Ser referência na recuperação ambiental e no apoio às comunidades, contribuindo para um futuro sustentável onde a natureza e as populações locais coexistam.</p>
+                            </li>
+                            <li className="d-md-flex">
+                                <h3>Nossos <br />valores</h3>
+                                <ul className="px-md-5 w-75 fs-4">
+                                    <li>Solidariedade;</li>
+                                    <li>Empatia;</li>
+                                    <li>Transparência.</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="section-title__nossos-propositos rounded-end-4 d-md-flex align-items-center">
+                        <h2 className="title__nossos-propositos ps-4 pt-5 pt-md-0 px-md-5">Nossos <br />propósitos</h2>
                     </div>
                 </div>
             </div>
--- src/components/QuemSomos/Header/index.jsx
+++ src/components/QuemSomos/Header/index.jsx
@@ -5,13 +5,13 @@ import Style from "./style.css";
 export default function HeaderQss() {
     return(
         <div className={Style}>
-            <div className="headerQs pb-5 pt-5 rounded-3">
+            <div className="headerQs pb-5 pt-5 rounded-bottom-4">
                 <div className="row align-items-center mt-5 ms-0 me-0">
-                    <div className="col-4 ps-5 d-flex row justify-content-center align-items-center">
-                        <h1 className="text-start pt-5 ps-5 display-3 h1Qs">Quem Somos</h1>
-                        <p className="text-start ps-5 pt-2 fs-5 textoQs">Nossa história é a força da esperança e da transformação. Descubra como cada passo é uma jornada de respeito, proteção e renovação</p>
+                    <div className="col-12 col-md-4 ps-md-5 d-flex flex-column ">
+                        <h1 className="text-start pt-5 ps-md-5 display-3 h1Qs">Quem Somos</h1>
+                        <p className="text-start ps-md-5 pt-2 fs-4 textoQs">Nossa história é a força da esperança e da transformação. Descubra como cada passo é uma jornada de respeito, proteção e renovação</p>
                     </div>
-                    <div className="col-8 ps-5 pt-5">
+                    <div className="col-12 col-md-8 ps-md-5 pt-5 px-auto">
                         <img src={TestQs} className="rounded mx-auto d-block img-fluid imgTest " alt="" />
                     </div>
                 </div>
