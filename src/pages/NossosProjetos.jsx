b0e0c69cd15400d4ea358b038dfe3266ccd09fa2
Julie <juliegodoi@gmail.com>
2024-07-20 20:51:18 -0300

    style: finaliza pag nossos projetos

--- src/pages/NossosProjetos.jsx
+++ src/pages/NossosProjetos.jsx
@@ -1,13 +1,16 @@
 import Container from '../components/Global/Container/index.jsx';
 import Navbar from '../components/Global/Navbar/index.jsx';
 import Footer from '../components/Global/Footer/index.jsx';
+import Header from '../components/NossosProjetos/Header/index.jsx';
+import Projetos from '../components/NossosProjetos/Projetos/index.jsx';
 
 export default function NossosProjetos() {
   return (
     <>
       <Navbar/>
       <Container>
-          <h1 className='m-5 pt-5'>Nossos Projetos</h1>
+        <Header/>
+        <Projetos/>
         <Footer/>
       </Container>
     </>
